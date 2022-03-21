ceda5bdd15acdb34d334ebf82a778778c63dd5b5
zhangchao <madril1130@gmail.com>
2022-03-21 10:55:09 +0800

    [add]:add create oauth2 url

--- src/Service/AuthService.php
+++ src/Service/AuthService.php
@@ -4,5 +4,15 @@ namespace WorkWechat\Service;
 
 class AuthService extends BaseService
 {
+    public static function getRedirectUrl(string $corpId, string $url): string
+    {
+        return 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' . $corpId . '&redirect_uri=' .
+            urlencode($url) . '&response_type=code&scope=snsapi_base&state=#wechat_redirect';
+    }
 
+    public static function getQrCodeUrl(string $corpId, string $agentId, string $url, string $state = ''): string
+    {
+        return 'https://open.work.weixin.qq.com/wwopen/sso/qrConnect?appid=' . $corpId . '&agentid=' . $agentId .
+            '&redirect_uri=' . urlencode($url) . '&state=' . $state;
+    }
 }
